40db7dffe70e4b026d409a6302c2213f861762ea
Harsh-Sisodia857 <Harsh2012007@akgec.ac.in>
2022-04-06 21:38:47 +0530

    Adding probs

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,42 +1,10 @@
 import React from 'react';
 
-class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            price : 999,
-            title : 'Mobile Phone',
-            qty : 1,
-            img : ''
-        }
-    }
+class CartItem extends React.Component {
 
-increaseQuantity = () =>{
-   // this.state.qty += 1;
-    // console.log('this', this.state);
-    // setState form 1
-    // this.setState({
-    //   qty: this.state.qty + 1
-    // });
-
-    // setState form 2 - if prevState required use this
-    this.setState((prevState) => {
-        return {
-          qty: prevState.qty + 1
-        }
-    });
-}
-
-decreaseQuantity = () =>{
-    if(this.state.qty > 0){
-        this.setState({
-            qty : this.state.qty - 1
-        })
-    }
-}
     render(){
         // object destructing
-        const {price,title,qty} = this.state;
+        const {price,title,qty} = this.props.product;
         return(
             <div className = 'cart-item'>
                 <div className='left-block'>
@@ -51,13 +19,13 @@ decreaseQuantity = () =>{
                         <img alt = "increase" className='action-icons' onClick = {this.increaseQuantity} src = "https://cdn-icons.flaticon.com/png/512/1008/premium/1008978.png?token=exp=1649248621~hmac=89b6e59cf6c0397a14c629d681bf9a91" />
                         <img alt = "decrease" className='action-icons' onClick={this.decreaseQuantity} src = "https://cdn-icons.flaticon.com/png/512/2920/premium/2920674.png?token=exp=1649248749~hmac=3ce9c394e80ccc130de6a4dea6e5dc65" />
                         <img alt = "delete" className='action-icons' src = "https://cdn-icons-png.flaticon.com/512/565/565491.png" />
-
                     </div>
                 </div>
 
             </div>
         )
     }
+
 }
 
 const styles = {
@@ -68,5 +36,4 @@ const styles = {
         
     }
 }
-
 export default CartItem;
